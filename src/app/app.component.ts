0ce1dc7a71c26202c6f07e07a9538fbbd7a5134f
nicolas-rm <65315133+nicolas-rm@users.noreply.github.com>
2020-08-31 20:42:01 -0500

    INICIO DEL PROYECTO, ANGULAR PURO SIN LA APLICACION DEL PATRON REDUX

---
+++ src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  contador: number;
+
+  constructor() {
+    this.contador = 10;
+  }
+
+  incrementar() {
+    this.contador += 1;
+  }
+
+  decrementar() {
+    if (this.contador === 0) {
+      return;
+    } else {
+      this.contador -= 1;
+    }
+
+  }
+}
